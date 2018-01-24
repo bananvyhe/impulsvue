2661a1728f1ad42d88b9b699aedda3491b260965
bananvyhe <loadonden@yahoo.com>
2018-01-24 22:31:08 +0500

    has working without tb adapter :\

---
+++ build/js/app.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      message: "Hello Vue123!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
