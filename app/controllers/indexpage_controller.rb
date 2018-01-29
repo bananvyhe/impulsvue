56c9773966499a5d4d3eac80566bab06aec367c4
bananvyhe <loadonden@yahoo.com>
2018-01-29 19:45:43 +0500

    install devise axios, create indexpage, sign up section

---
+++ app/controllers/indexpage_controller.rb
@@ -0,0 +1,7 @@
+class IndexpageController < ApplicationController
+	 # before_filter :authenticate_user!
+
+  def index
+  	 
+  end	
+end
---
+++ test/controllers/indexpage_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IndexpageControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
