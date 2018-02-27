3334275254d23a004dab6729319cc3f9602ecbdc
bananvyhe <loadonden@yahoo.com>
2018-02-27 22:23:42 +0500

    begin pageproof

---
+++ build/js/head.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="head">
+		<transition name="fade" appear>
+      <div class="first">
+       <nobr>секция 1</nobr>
+      </div>    
+    </transition>
+    <div class="second">секция 2
+    </div>
+    <div class="third">
+      <nobr>секция 3</nobr>
+    </div> 
+
+  </div>
+</template>
+
+<script>
+console.log('head.vue');
+
+export default {
+   
+  data: function () {
+    return {
+ 
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.head {
+	background-image: url(../../app/assets/images/ui_02.jpg);
+	background-repeat: no-repeat;
+	background-position: center bottom;
+	height: 160px;
+	background-color: #ada;
+	div {
+  	lost-column: 1/3;
+	}
+}
+.telpanel {
+    justify-content: center;  
+  }
+  .first {
+    background-color: #dad;
+  }
+  .second {
+   background-color: #ada;
+  }
+  .third {
+    background-color: #dad;
+  }
+ 
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+ 
+ 
+</style>
--- src/js/appbuild.js
+++ src/js/appbuild.js
@@ -3,26 +3,26 @@ import axios from 'axios'
 import Vue from 'vue/dist/vue.esm'
 import store from './store'
 import App from './telpanel.vue' 
+import Head from './head.vue' 
 // Vue.use(TurbolinksAdapter)
-
-console.log('firstappb');
 document.addEventListener('DOMContentLoaded', () => {
  let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
   axios.defaults.headers.common['X-CSRF-Token'] = token
   axios.defaults.headers.common['Accept'] = 'application/json'  
   //пример обмена vue-rails 
-  console.log('secondappb');
   var element = document.getElementById("team-form")
- 	console.log(element);
-    var app = new Vue({
-      el: element,
-      template: '<App/>',
-      components: {App},
-      store,
-        data: function() {
-        return {   }
-      } 
-    }) 
-   console.log('aftervueinst');
-  //end
+  var app = new Vue({
+    el: element,
+    template: '<App/>',
+    components: {App},
+    store,
+      data: function() {
+      return {   }
+    } 
+  }) 
+  var head = new Vue({
+    el: '#head',
+    render: h => h(Head)
+    
+  }) 
 })
