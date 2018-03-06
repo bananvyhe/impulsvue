69ebbd14e3566d25298b324e92e8e6a2106ab705
bananvyhe <loadonden@yahoo.com>
2018-03-06 14:43:31 +0500

    childs controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 	resources :teams
+	resources :childs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "indexpage#index"
 end
