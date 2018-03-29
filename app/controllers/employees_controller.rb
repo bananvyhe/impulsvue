761951ff1aff170782a6cfc955f8735e6884f34a
bananvyhe <loadonden@yahoo.com>
2018-03-29 14:48:08 +0500

    make for ckeeditorstyle rails asset_path to css in js file

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -20,12 +20,32 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: 'employee was successfully created.' }
+        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
         format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new }
         format.json { render json: @employee.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @employee.update(employee_params)
+        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
+        format.json { render :show, status: :ok, location: @employee }
+      else
+        format.html { render :edit }
+        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    respond_to do |format|
+      format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
