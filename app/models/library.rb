14410637aa39546caf500d2b500890da03fbede1
bananvyhe <loadonden@yahoo.com>
2018-05-02 19:48:23 +0500

    strip_tags slider view desc

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,4 @@
 class Library < ApplicationRecord
+	mount_uploader :file, FileUploader
+
 end
--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,31 +1,10 @@
-class NewspicUploader < CarrierWave::Uploader::Base
+class FileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  include CarrierWave::MiniMagick
- 
-  process resize_to_limit: [1200, 800]
-
-  version :thumb do
-    process :crop
-    resize_to_fill(225, 150)
-  end
-  def crop
-    if model.crop_x.present?
-      resize_to_limit(1200, 800)
-      manipulate! do |img|
-        x = model.crop_x.to_i
-        y = model.crop_y.to_i
-        w = model.crop_w.to_i
-        h = model.crop_h.to_i
-        # [[w, h].join('x'),[x, y].join('+')].join('+') => "wxh+x+y"
-        img.crop([[w, h].join('x'),[x, y].join('+')].join('+'))
-      end
-    end
-  end 
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
