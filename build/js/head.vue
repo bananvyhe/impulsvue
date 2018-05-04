38348d8b64ae9353e268dbfc25eda002f682d728
bananvyhe <loadonden@yahoo.com>
2018-05-04 15:25:36 +0500

    head pp

--- build/js/head.vue
+++ build/js/head.vue
@@ -4,21 +4,36 @@
       <div class="first">
       </div>    
     </transition>
-    <div class="second">
-      
+    <div class="second" v-if="contacts && contacts.length">
+      <div v-for="contact of contacts">
+        {{contact.mail}}
+      </div>
     </div>
-    <div class="third">
-      <nobr>секция 3</nobr>
-    </div> 
+    <div class="third" v-if="contacts && contacts.length">
+      <div v-for="contact of contacts">
+        {{contact.adress}}
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   data: function () {
     return {
- 
+      contacts: []
     }
-  }
+  },
+  created() {
+    axios.get('/contacts')
+    .then(response => {
+      this.contacts = response.data
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  },
 }
 </script>
 <style scoped>
@@ -34,13 +49,21 @@ export default {
   background-size: contain;
   margin: 3em 1em -1em 1em;
 	lost-column: 1/2 2 0.5em;
-   
 }
-.second {lost-column: 1/4 4 0.5em;
- background-color: #ada;
+.second { border-right: 1px solid black;
+  display: flex;
+  lost-column: 1/4 4 1em;
+  justify-content: flex-end;
+  align-self: center;
+  margin-top: 4em;
+  padding: 0.5em;
 }
-.third {lost-column: 1/4 4 0.5em;
-  background-color: #dad;
+.third {
+  display: flex;
+  align-self: center;
+  lost-column: 1/4 4 1em;
+  margin-top: 4em;
+  padding: 0.5em;
 }
 .fade-enter {
   opacity: 0;
